347baf18dbde77ad992bcd92e76958f460f4613b
Samuel Ryzycki <samuel@ryzycki.com>
2014-03-07 00:22:01 +0000

    Move M12.Boostrap files over

---
+++ Classes/M12/Foundation/Package.php
@@ -0,0 +1,30 @@
+<?php
+namespace M12\Foundation;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3CR".               *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Package\Package as BasePackage;
+
+/**
+ * The M12.Foundation Package
+ */
+class Package extends BasePackage {
+
+	/**
+	 * Invokes custom PHP code directly after the package manager has been initialized.
+	 *
+	 * @param \TYPO3\Flow\Core\Bootstrap $bootstrap The current bootstrap
+	 * @return void
+	 */
+	public function boot(\TYPO3\Flow\Core\Bootstrap $bootstrap) {
+//		$dispatcher = $bootstrap->getSignalSlotDispatcher();
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 M12.Foundation
-==============
+=============
+
+TYPO3 Neos package implementing Foundation 5 elements.
