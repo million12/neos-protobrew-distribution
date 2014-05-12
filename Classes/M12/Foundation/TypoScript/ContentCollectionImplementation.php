6b7d9763b4ac0f5f8f56c7bcbf576b6fd676c149
ryzy <marcin@ryzycki.com>
2014-05-12 21:22:00 +0100

    ContentCollectionImplementation: set default properties for tab item (active tab) and column (size)
    Rel. TS-98, TS-81

--- Classes/M12/Foundation/TypoScript/ContentCollectionImplementation.php
+++ Classes/M12/Foundation/TypoScript/ContentCollectionImplementation.php
@@ -13,10 +13,16 @@ namespace M12\Foundation\TypoScript;
 
 use TYPO3\TYPO3CR\Domain\Model\Node;
 use TYPO3\Neos\TypoScript\ContentCollectionImplementation as NeosContentCollectionImplementation;
-use TYPO3\TypoScript\TypoScriptObjects\AbstractCollectionImplementation;
+use TYPO3\Flow\Annotations as Flow;
 
 /**
  * Overrides Neos ContentCollections
+ *
+ * @todo This is an artifact after times when this implementation was
+ *       removing wrapping div.content-collection. Now this functionality
+ *       is moved to M12.Utils:WrapRemover.
+ * @todo Method setDefaultProperties() should be removed to some more
+ *       appropriate place (eg. triggered by event when node is initially inserted).
  */
 class ContentCollectionImplementation extends NeosContentCollectionImplementation {
 
@@ -24,6 +30,12 @@ class ContentCollectionImplementation extends NeosContentCollectionImplementatio
 	 * @var array
 	 */
 	protected $settings;
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Persistence\PersistenceManagerInterface
+	 */
+	protected $persistenceManager;
 
 	/**
 	 * M12.FoundationGrid settings
@@ -64,14 +76,44 @@ class ContentCollectionImplementation extends NeosContentCollectionImplementatio
 	 * @param Node $contentCollectionNode
 	 */
 	protected function setDefaultProperties(Node $contentCollectionNode) {
-		$parentNodeType = $contentCollectionNode->getParent()->getNodeType()->getName();
+		$parentNode = $contentCollectionNode->getParent();
+		$parentNodeType = $parentNode->getNodeType()->getName();
 		$nodeType = $contentCollectionNode->getNodeType()->getName();
+
+		switch ($parentNodeType) {
+			// Set 1st tab as active tab (otherwise all are hidden and it's hard to add any content there.
+			case 'M12.Foundation:Tabs2':
+			case 'M12.Foundation:Tabs3':
+			case 'M12.Foundation:Tabs4':
+			case 'M12.Foundation:Tabs5':
+			case 'M12.Foundation:Tabs6':
+				// already set?
+				if ($contentCollectionNode->getProperty('activeTab'))
+					break;
+
+				// otherwise check if any child has it set
+				/** @var Node[] $children */
+				if (!($children = $parentNode->getChildNodes()))
+					break;
+
+				foreach ($children as $node) {
+					if ($node->getProperty('activeTab'))
+						break;
+				}
+
+				// pick up the 1st tab and mark it as active
+				$node = array_shift($children);
+				$node->setProperty('activeTab', true);
+				$this->persistenceManager->persistAll();
+				break;
+		}
 
 		switch ($nodeType) {
 			case 'M12.Foundation:Column':
 			case 'M12.Foundation:ColumnEnd':
 				$gridSize = $this->settings['gridSize'];
-				$columns = (int)str_replace('M12.Foundation:GridColumns', '', $parentNodeType);
+				// extract num of columns from 'M12.Foundation:GridRowXCol' node type
+				$columns = (int)substr($parentNodeType, strlen('M12.Foundation:GridRow'), 1);
 
 				// In case M12.Foundation:Column has been placed as a child of element
 				// different than M12.Foundation:GridColumnsX, make sure we do not divide by 0!
@@ -86,6 +128,7 @@ class ContentCollectionImplementation extends NeosContentCollectionImplementatio
 					$keys = array_keys($sizeSettings);
 					$property = $keys[0];
 					$contentCollectionNode->setProperty($property, 'small-'.$defaultColumns);
+					$this->persistenceManager->persistAll();
 				}
 				break;
 		}
