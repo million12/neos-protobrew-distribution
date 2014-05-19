d03af6a6df4c6fd16307d8cfd16b92979b1931dc
ryzy <marcin@ryzycki.com>
2014-05-19 23:55:59 +0100

    TS-97: Possibility to add custom attributes to any content element

--- Classes/M12/Foundation/TypoScript/AttributesImplementation.php
+++ Classes/M12/Foundation/TypoScript/AttributesImplementation.php
@@ -23,6 +23,11 @@ use TYPO3\Flow\Annotations as Flow;
 class AttributesImplementation extends NeosAttributesImplementation {
 
 	/**
+	 * Key name under which custom user attributes might be available
+	 */
+	const CUSTOM_USER_ATTRIBUTES_KEY = 'customUserAttributes';
+
+	/**
 	 * @var array
 	 */
 	protected $attributes;
@@ -36,6 +41,8 @@ class AttributesImplementation extends NeosAttributesImplementation {
 	 * @return string
 	 */
 	public function evaluate() {
+		$this->parseCustomUserAttributes();
+
 		$attributes = array();
 		$renderedAttributes = '';
 		foreach (array_keys($this->properties) as $attributeName) {
@@ -62,6 +69,31 @@ class AttributesImplementation extends NeosAttributesImplementation {
 		$this->renderedAttributes = $renderedAttributes;
 
 		return $this;
+	}
+
+	/**
+	 * Get customUserAttributes, parse them (one attribute per line in format attribute=value)
+	 * and inject them to internal TS values, so they can be fetched in evaluate() method.
+	 */
+	protected function parseCustomUserAttributes() {
+		if (($value = trim($this->tsValue(static::CUSTOM_USER_ATTRIBUTES_KEY)))) {
+			$customUserAttributes = explode(chr(10), $value);
+			foreach ($customUserAttributes as $line) {
+				if (empty($line)) continue;
+
+				$attributes = explode('=', $line);
+				$attributeName = trim(array_shift($attributes));
+				$attributeValue = trim(array_shift($attributes)); // support empty attributes too
+
+				// First: mark existence of custom attribute
+				// Second: add it to internal tsValueCache[], so $this->tsValue() can fetch it without evaluating.
+				$this->properties[$attributeName] = $attributeValue;
+				$this->tsValueCache[$this->path.'/'.$attributeName] = $attributeValue;
+			}
+
+			// unset original custom attribute
+			unset($this->properties[static::CUSTOM_USER_ATTRIBUTES_KEY]);
+		}
 	}
 
 	public function getAsString() {
