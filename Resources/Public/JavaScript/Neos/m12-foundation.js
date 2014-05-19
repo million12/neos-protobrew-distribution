6f583f1879646aa00a889f949b666261c2edb595
ryzy <marcin@ryzycki.com>
2014-05-19 20:33:16 +0100

    TS-113: Forms: LABEL inline editable / prevent default action after click

--- Resources/Public/JavaScript/Neos/m12-foundation.js
+++ Resources/Public/JavaScript/Neos/m12-foundation.js
@@ -1,1 +1,10 @@
 // Custom M12.Foundation code for Neos backend
+
+$(document).ready(function() {
+
+	// By default, after click on LABEL, focus is moved to related INPUT field.
+	// Prevent that default action while in edit mode (@see TS-113)
+	$('label').children('.neos-inline-editable').click(function(e) {
+		e.preventDefault();
+	});
+});
