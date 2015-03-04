117ca4d165ec209534272fda3d4c8ee6550bbec2
ryzy <marcin@ryzycki.com>
2015-03-04 16:21:01 +0000

    Neos 2.0-dev, docker-compose, improved build.sh, beard installation

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Site build script
 #
@@ -6,36 +6,51 @@
 # all necessary build steps (e.g. scripts minification, styles compilation etc).
 #
 
-BUILD_DIR="Packages/Sites/M12.FoundationSite/"
+CWD=$(pwd)
+PROJECT_NAME="NEOS-TYPOSTRAP-DISTRIBUTION"
+TYPO3_NEOS_PACKAGE_DIR="${CWD}/Packages/Application/TYPO3.Neos"
+M12_FOUNDATION_SITE_PACKAGE_DIR="${CWD}/Packages/Sites/M12.FoundationSite"
+
+function installNeosDevTools() {
+  cd "${TYPO3_NEOS_PACKAGE_DIR}/Scripts"
+  npm install
+  ./install-dev-tools.sh
+  cd $CWD
+}
+
+function buildM12FoundationSite() {
+  cd $M12_FOUNDATION_SITE_PACKAGE_DIR
+  bower install --allow-root # this script is called as root in '--preinstall' phase
+  npm install
+  gulp build --env=Production # build for production by default
+  cd $CWD
+}
+
 
 case $@ in
   #
   # This is called when container is being build (and this script is called with --preinstall param)
   #
   *--preinstall*)
-    echo "SITE BUILD SCRIPT: PREINSTALL"
+    echo "$PROJECT_NAME BUILD SCRIPT: PREINSTALL"
     
-    # Install required tools globally
-    npm install -g gulp bower
-    
-    # Install site packages
     set -e # exit with error if any of the following fail
-    cd $BUILD_DIR
-    bower install --allow-root
-    npm install
-    gulp build --env=Production
+    
+    # Install Beard
+    curl -s http://beard.famelo.com/ > "$CWD/bin/beard" && chmod +x "$CWD/bin/beard"
+    
+    installNeosDevTools
+    
+    buildM12FoundationSite
     ;;
  
   #
   # This is called when container launches (and script is called without param)
   #
   *)
-    echo "SITE BUILD SCRIPT"
-    cd $BUILD_DIR
-    bower install
-    npm install
-    gulp build --env=Production # build for production by default
+    echo "$PROJECT_NAME BUILD SCRIPT"
+    buildM12FoundationSite
     ;;
 esac
 
-echo "SITE BUILD SCRIPT completed."
+echo "$PROJECT_NAME BUILD SCRIPT completed."
