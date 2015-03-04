f228b7ffb4f3b973604e0fa8e587302a6eb95447
ryzy <marcin@ryzycki.com>
2015-03-04 16:24:12 +0000

    Running beard patch with custom changes from gerrit + build fix

--- build.sh
+++ build.sh
@@ -7,18 +7,24 @@
 #
 
 CWD=$(pwd)
+RUN_BEARD=${RUN_BEARD:=false}
+
 PROJECT_NAME="NEOS-TYPOSTRAP-DISTRIBUTION"
 TYPO3_NEOS_PACKAGE_DIR="${CWD}/Packages/Application/TYPO3.Neos"
 M12_FOUNDATION_SITE_PACKAGE_DIR="${CWD}/Packages/Sites/M12.FoundationSite"
 
 function installNeosDevTools() {
+  echo "Installing Neos dev tools..."
+  mkdir -p /data/www && chown www:www /data/www # needed for `npm install for its cache, called from ./install-dev-tools.sh
   cd "${TYPO3_NEOS_PACKAGE_DIR}/Scripts"
-  npm install
-  ./install-dev-tools.sh
+  chown -R www:www . # Fix perms for current dir as they are not set to www user yet. Needed for ./install-dev-tools.sh which cannot be run as root (bower error)
+  su www -c "./install-dev-tools.sh" # Run as www user: Bower will exit if it's run as root user
+  rm -rf /data/www # This function is callled only during docker build. We don't need to embed this dir in the image...
   cd $CWD
 }
 
 function buildM12FoundationSite() {
+  echo "Building M12.FoundationSite..."
   cd $M12_FOUNDATION_SITE_PACKAGE_DIR
   bower install --allow-root # this script is called as root in '--preinstall' phase
   npm install
@@ -32,11 +38,12 @@ case $@ in
   # This is called when container is being build (and this script is called with --preinstall param)
   #
   *--preinstall*)
-    echo "$PROJECT_NAME BUILD SCRIPT: PREINSTALL"
+    echo && echo "$PROJECT_NAME BUILD SCRIPT: PREINSTALL"
     
     set -e # exit with error if any of the following fail
     
     # Install Beard
+    echo "Installing Beard..."
     curl -s http://beard.famelo.com/ > "$CWD/bin/beard" && chmod +x "$CWD/bin/beard"
     
     installNeosDevTools
@@ -48,9 +55,15 @@ case $@ in
   # This is called when container launches (and script is called without param)
   #
   *)
-    echo "$PROJECT_NAME BUILD SCRIPT"
+    echo && echo "$PROJECT_NAME BUILD SCRIPT"
+    git config --global user.email "www@build.user" &&  git config --global user.name "WWW User"
+    
     buildM12FoundationSite
-    ;;
+    
+    if [ "${RUN_BEARD^^}" = TRUE ]; then
+      bin/beard patch
+      grunt --gruntfile=Packages/Application/TYPO3.Neos/Scripts/Gruntfile.js build
+    fi
 esac
 
 echo "$PROJECT_NAME BUILD SCRIPT completed."
